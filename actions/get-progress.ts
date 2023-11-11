a1b3accdf46e237aabff90f1a764792689d0ee0c
EternityNightV <143103542+EternityNightV@users.noreply.github.com>
2023-11-11 20:15:53 +0300

    render courses + render progress start

---
+++ actions/get-progress.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db";
+
+export const getProgress = async(
+    userId : string,
+    courseId : string
+) : Promise<number> => {
+    try {
+        const publsihedChapters = await db.chapter.findMany({
+            where : {
+                courseId : courseId,
+                isPublished : true
+            },
+            select : {
+                id : true
+            }
+        })
+
+        const publsihedChaptersIds = publsihedChapters.map((chapter) => chapter.id)
+
+        const validCompletedChapters = await db.userProgress.count({
+            where : {
+                userId : userId,
+                chapterId : {
+                    in : publsihedChaptersIds
+                },
+                isCompleted : true
+            }
+        })
+
+        const progressPercentage = (validCompletedChapters / publsihedChapters.length) * 100
+
+        return progressPercentage
+    } catch (error) {
+        console.log('[GET_PROGRESS]', error);
+        return 0
+    }
+}
